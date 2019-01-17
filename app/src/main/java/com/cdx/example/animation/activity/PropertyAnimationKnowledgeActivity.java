4bd8fade56c3daa113833caf733157b70aaf8e22
陈德祥 <937682896@qq.com>
2019-01-17 22:50:12 +0800

    1、属性动画。

--- app/src/main/java/com/cdx/example/animation/activity/PropertyAnimationKnowledgeActivity.java
+++ app/src/main/java/com/cdx/example/animation/activity/PropertyAnimationKnowledgeActivity.java
@@ -1,10 +1,6 @@
 package com.cdx.example.animation.activity;
 
-import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
-
 import com.cdx.answerlibrary.activity.BaseAnswerListActivity;
-import com.cdx.example.animation.R;
 
 public class PropertyAnimationKnowledgeActivity extends BaseAnswerListActivity {
 
